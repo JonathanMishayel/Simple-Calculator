0b9b2be7cd9a8148b648aa1f8ac0f451be9d818c
Jonathan Mishayel <jonathanmishayel2000@gmail.com>
2025-08-15 00:10:31 +0530

    Initial commit

---
+++ calculator_dp1.py
@@ -0,0 +1,83 @@
+# calculator_dp1
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a / b
+
+def power(a, b):
+    return a ** b
+
+def remainder(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a % b
+
+def select_op(choice):
+    if choice in ['+', '-', '*', '/', '^', '%']:
+        try:
+            first = input("Enter first number: ")
+            if first == '$':
+                return
+            a = float(first)
+        except ValueError:
+            print("Not a valid number,please enter again")
+            return
+
+        try:
+            second = input("Enter second number: ")
+            if second == '$':
+                return
+            b = float(second)
+        except ValueError:
+            print("Not a valid number,please enter again")
+            return
+
+        try:
+            if choice == '+':
+                print(f"{a} + {b} = {add(a, b)}")
+            elif choice == '-':
+                print(f"{a} - {b} = {subtract(a, b)}")
+            elif choice == '*':
+                print(f"{a} * {b} = {multiply(a, b)}")
+            elif choice == '/':
+                print(f"{a} / {b} = {divide(a, b)}")
+            elif choice == '^':
+                print(f"{a} ^ {b} = {power(a, b)}")
+            elif choice == '%':
+                print(f"{a} % {b} = {remainder(a, b)}")
+        except ZeroDivisionError:
+            print("Something Went Wrong")
+        except:
+            print("Something Went Wrong")
+    elif choice == '#':
+        print("Done. Terminating")
+        return 'terminate'
+    elif choice == '$':
+        return
+    else:
+        print("Unrecognized operation")
+
+while True:
+    print("Select operation.")
+    print("1.Add      : +")
+    print("2.Subtract : -")
+    print("3.Multiply : *")
+    print("4.Divide   : /")
+    print("5.Power    : ^")
+    print("6.Remainder: %")
+    print("7.Terminate: #")
+    print("8.Reset    : $")
+    user_input = input("Enter choice(+, -, *, /, ^, %, #, $): ")
+    result = select_op(user_input)
+    if result == 'terminate':
+        break
